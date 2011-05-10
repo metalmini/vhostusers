4a75e939a5f75e336540904b3739ec6e75045a2c
Michael Schouman <metalmini@gmail.com>
2011-05-10 05:15:48 -0700

    Optimized for cent OS and added ftpusers

--- vhostusers.sh
+++ vhostusers.sh
@@ -1,8 +1,7 @@
 #/bin/bash
-#Script to add/remove vhost users on a Linux Apache2 Webserver by Michael V.2.2 19-08-2005
-#
-#Fill in your own stuff
-#
+#Script to add/remove vhost users on a Linux Apache2 Webserver by Michael V.2.2 19-08-2005 # #Fill in your own stuff #
+
+#set -x
 
 ADMINMAIL=
 ADMINNAME=
@@ -28,46 +27,48 @@ echo -n "also send an email to:"
 read EMAIL
 
 echo -n "user expire (YYYY-M-D):"
-read EXPIRE 
+read EXPIRE
 
 #
-#This will generate a random password for the new vhost
-#
+#This will generate a random password for the new vhost #
 
 MATRIX="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 LENGTH="8"
 while [ ${n:=1} -le $LENGTH ]
 do
-	GENPASS="$GENPASS${MATRIX:$(($RANDOM%${#MATRIX})):1}"
-	let n+=1
+GENPASS="$GENPASS${MATRIX:$(($RANDOM%${#MATRIX})):1}"
+let n+=1
 done
 
-sudo useradd $USERNAME -m -s /bin/bash -p $(openssl passwd -crypt $GENPASS) 
+sudo useradd $USERNAME -m -s /bin/bash -p $(openssl passwd -crypt $GENPASS)
 
-	mkdir /home/$USERNAME/log
-	mkdir /home/$USERNAME/www
-	touch /home/$USERNAME/log/access_log
-	touch /home/$USERNAME/log/error_log
+mkdir /home/$USERNAME/log
+mkdir /home/$USERNAME/www
+touch /home/$USERNAME/log/$USERNAME.access.log
+touch /home/$USERNAME/log/$USERNAME.error.log
 
-chown -R $USERNAME.www-data /home/$USERNAME/www
-chown -R $USERNAME.$ADMINNAME /home/$USERNAME/log
-chmod -R 755 /home/$USERNAME
+chown -R $USERNAME:apache /home/$USERNAME/www chown -R $USERNAME:apache /home/$USERNAME/log chmod -R 755 /home/$USERNAME
 
+echo $USERNAME >> /etc/vsftpd/user_list
+echo $USERNAME >> /etc/vsftpd/chroot_list
 
-echo "<VirtualHost *>
+echo "<VirtualHost *:80>
 ServerAdmin $ADMINMAIL
 DocumentRoot /home/$USERNAME/www
 ServerName $DOMEIN
-TransferLog /home/$USERNAME/log/access_log
-ErrorLog /home/$USERNAME/log/error_log
-</VirtualHost>" >> /etc/apache2/conf/$DOMEIN.conf
+ServerAlias www.$DOMEIN
+<Directory />
+Options FollowSymLinks
+AllowOverride None
+</Directory>
+<Directory /home/$USERNAME/www/>
+Options Indexes FollowSymLinks MultiViews AllowOverride All Order allow,deny allow from all </Directory> TransferLog /home/$USERNAME/log/$USERNAME.access.log
+ErrorLog /home/$USERNAME/log/$USERNAME.error.log
+</VirtualHost>" >> /etc/httpd/conf/$DOMEIN.conf
 
-echo "<?php phpinfo(); ?>" >> /home/$USERNAME/www/index.php
-chown $USERNAME:$USERNAME /home/$USERNAME/www/index.php
+echo "<?php phpinfo(); ?>" >> /home/$USERNAME/www/index.php chown $USERNAME:$USERNAME /home/$USERNAME/www/index.php
 
-echo "Include /etc/apache2/conf/$DOMEIN.conf" >> /etc/apache2/apache2.conf
-
-sudo /usr/sbin/apache2 -k graceful
+echo "Include /etc/httpd/conf/$DOMEIN.conf" >> /etc/httpd/conf/httpd.conf
 
 
 echo "username: $USERNAME"
@@ -76,17 +77,9 @@ echo " "
 echo "MVG $ADMINNAME"
 echo " "
 
-#You can activate the mail option by removing the # next to the echo
-#This optionwil e-mail the account stuff to you and someone else if you fill in the mail adress at the beginning of the script
-#
-#echo -e "Hi there, \n\nThis is your new username & password for your ftp account at $DOMEIN \n\nusername: $USERNAME \npasswd: $GENPASS \n\n$ADMINMAIL \n$ADMINDOMAIN \n\nIn case of lost password, send an e-mail to $ADMINMAIL\n
+#You can activate the mail option by removing the # next to the echo #This optionwil e-mail the account stuff to you and someone else if you fill in the mail adress at the beginning of the script # echo -e "Hi there, \n\nThis is your new username & password for your ftp account at $DOMEIN \n\nusername: $USERNAME \npasswd: $GENPASS \n\n$ADMINMAIL \n$ADMINDOMAIN \n\nIn case of lost password, send an e-mail to $ADMINMAIL\n
 
-#You are fully responsible for any information or file supplied on your
-#website. Is not Allowed to publish any copyrighted material that is not owned
-#by yourself. Is not allowed to post any information which is vulgar,
-#harassing, hateful, threatening, invading of others privacy, sexually
-#oriented, or violates any laws, child porn would be reported to the police.
-#Just be nice & enjoy the net. \n\nDo not reply to this mail" | mail -s "$ADMINDOMAIN new passwd for user $USERNAME" "$ADMINMAIL,$EMAIL"
+Just be nice & enjoy the net. \n\nDo not reply to this mail" | mail -s "$ADMINDOMAIN new passwd for user $USERNAME" "$ADMINMAIL,$EMAIL"
 
 }
 
@@ -109,15 +102,12 @@ fi
 echo -n "domein:"
 read DOMEIN
 
-userdel -r $USERNAME 
-rm /etc/apache2/conf/$DOMEIN.conf
-
-sudo /usr/sbin/apache2 -k graceful
+userdel -r $USERNAME
+rm /etc/httpd/conf/$DOMEIN.conf
 
 echo "user: $USERNAME has been removed from the server"
 
-#remove # if you want to recieve mail that the user is removed
-# | mail -s "$ADMINDOMAIN user removed" "$ADMINMAIL"
+#remove # if you want to recieve mail that the user is removed # | mail -s "$ADMINDOMAIN user removed" "$ADMINMAIL"
 
 }
 
@@ -150,7 +140,7 @@ MATRIX="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 LENGTH="8"
 while [ ${n:=1} -le $LENGTH ]
 do
-        GENPASS="$GENPASS${MATRIX:$(($RANDOM%${#MATRIX})):1}"
+GENPASS="$GENPASS${MATRIX:$(($RANDOM%${#MATRIX})):1}"
         let n+=1
 done
 
@@ -159,37 +149,24 @@ usermod -p $(openssl passwd -crypt $GENPASS) $USERNAME
 echo "username: $USERNAME"
 echo "passwd: $GENPASS"
 
-#remove als the # to recieve mail with the new password 
-#
-#echo -e "Hi there, \n\nThis is your new password for your ftp account at $DOMEIN \n\nusername:$USERNAME \npasswd: $GENPASS \n\n$ADMINNAME \n$ADMINDOMAIN \n\nIn case of lost password, send
- an
-#e-mail to $ADMINMAIL \n
-#You are fully responsible for any information or file supplied on your
-#website. Is not Allowed to publish any copyrighted material that is not owned
-#by yourself. Is not allowed to post any information which is vulgar,
-#harassing, hateful, threatening, invading of others privacy, sexually
-#oriented, or violates any laws, child porn would be reported to the police.
-#Just be nice & enjoy the net. \n\nDo not reply to this mail" | mail -s "$ADMINDOMAIN new passwd for
-# user $USERNAME" "$ADMINMAIL,$EMAIL" 
+#remove als the # to recieve mail with the new password # #echo -e "Hi there, \n\nThis is your new password for your ftp account at $DOMEIN \n\nusername:$USERNAME \npasswd: $GENPASS \n\n$ADMINNAME \n$ADMINDOMAIN \n\nIn case of lost password, send  an #e-mail to $ADMINMAIL \n #You are fully responsible for any information or file supplied on your #website. Is not Allowed to publish any copyrighted material that is not owned #by yourself. Is not allowed to post any information which is vulgar, #harassing, hateful, threatening, invading of others privacy, sexually #oriented, or violates any laws, child porn would be reported to the police.
+#Just be nice & enjoy the net. \n\nDo not reply to this mail" | mail -s "$ADMINDOMAIN new passwd for # user $USERNAME" "$ADMINMAIL,$EMAIL"
 
 }
 case "$1" in
         -[cC])
-                adduser 
+                adduser
         ;;
         -[dD])
                 deleteuser
         ;;
-        -[pP])  passwdrotator 
+        -[pP]) passwdrotator
         ;;
         *)
-                echo "  $ADMINDOMAIN add/remove vhost users & rotate passwd"
-                echo "  To create use: -c -C option" 
-                echo "  To remove use: -d -D option" 
-                echo "  To rotate passwd use: -p -P option" 
+                echo " $ADMINDOMAIN add/remove vhost users & rotate passwd"
+                echo " To create use: -c -C option"
+                echo " To remove use: -d -D option"
+                echo " To rotate passwd use: -p -P option"
         ;;
 esac
-  
-  
 exit 0
-
