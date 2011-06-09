a081007a6aeea31c581be973d36280108bb2b9e6
Michael Schouman <metalmini@gmail.com>
2011-06-09 06:54:30 -0700

    minor bugs fixed

--- vhostusers.sh
+++ vhostusers.sh
@@ -1,5 +1,8 @@
 #/bin/bash
-#Script to add/remove vhost users on a Linux Apache2 Webserver by Michael V.2.2 19-08-2005 # #Fill in your own stuff #
+#Script to add/remove vhost users on a Linux Apache2 Webserver by Michael V.2.2 19-08-2005
+#
+#Fill in your own stuff
+#
 
 #set -x
 
@@ -30,7 +33,8 @@ echo -n "user expire (YYYY-M-D):"
 read EXPIRE
 
 #
-#This will generate a random password for the new vhost #
+#This will generate a random password for the new vhost
+#
 
 MATRIX="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 LENGTH="8"
@@ -40,10 +44,16 @@ GENPASS="$GENPASS${MATRIX:$(($RANDOM%${#MATRIX})):1}"
 let n+=1
 done
 
-sudo useradd $USERNAME -m -s /bin/bash -p $(openssl passwd -crypt $GENPASS) mkdir /home/$USERNAME/log mkdir /home/$USERNAME/www touch /home/$USERNAME/log/$USERNAME.access.log
+sudo useradd $USERNAME -m -s /bin/bash -p $(openssl passwd -crypt $GENPASS)
+
+mkdir /home/$USERNAME/log
+mkdir /home/$USERNAME/www
+touch /home/$USERNAME/log/$USERNAME.access.log
 touch /home/$USERNAME/log/$USERNAME.error.log
 
-chown -R $USERNAME:apache /home/$USERNAME/www chown -R $USERNAME:apache /home/$USERNAME/log chmod -R 755 /home/$USERNAME
+chown -R $USERNAME:apache /home/$USERNAME/www
+chown -R $USERNAME:apache /home/$USERNAME/log
+chmod -R 755 /home/$USERNAME
 
 echo $USERNAME >> /etc/vsftpd/user_list
 echo $USERNAME >> /etc/vsftpd/chroot_list
@@ -58,11 +68,17 @@ Options FollowSymLinks
 AllowOverride None
 </Directory>
 <Directory /home/$USERNAME/www/>
-Options Indexes FollowSymLinks MultiViews AllowOverride All Order allow,deny allow from all </Directory> TransferLog /home/$USERNAME/log/$USERNAME.access.log
+Options Indexes FollowSymLinks MultiViews
+AllowOverride All
+Order allow,deny
+allow from all
+</Directory>
+TransferLog /home/$USERNAME/log/$USERNAME.access.log
 ErrorLog /home/$USERNAME/log/$USERNAME.error.log
 </VirtualHost>" >> /etc/httpd/conf/$DOMEIN.conf
 
-echo "<?php phpinfo(); ?>" >> /home/$USERNAME/www/index.php chown $USERNAME:$USERNAME /home/$USERNAME/www/index.php
+echo "<?php phpinfo(); ?>" >> /home/$USERNAME/www/index.php
+chown $USERNAME:$USERNAME /home/$USERNAME/www/index.php
 
 echo "Include /etc/httpd/conf/$DOMEIN.conf" >> /etc/httpd/conf/httpd.conf
 
@@ -73,7 +89,10 @@ echo " "
 echo "MVG $ADMINNAME"
 echo " "
 
-#You can activate the mail option by removing the # next to the echo #This optionwil e-mail the account stuff to you and someone else if you fill in the mail adress at the beginning of the script # echo -e "Hi there, \n\nThis is your new username & password for your ftp account at $DOMEIN \n\nusername: $USERNAME \npasswd: $GENPASS \n\n$ADMINMAIL \n$ADMINDOMAIN \n\nIn case of lost password, send an e-mail to $ADMINMAIL\n
+#You can activate the mail option by removing the # next to the echo
+#This optionwil e-mail the account stuff to you and someone else if you fill in the mail adress at the beginning of the script
+#
+echo -e "Hi there, \n\nThis is your new username & password for your ftp account at $DOMEIN \n\nusername: $USERNAME \npasswd: $GENPASS \n\n$ADMINMAIL \n$ADMINDOMAIN \n\nIn case of lost password, send an e-mail to $ADMINMAIL\n
 
 Just be nice & enjoy the net. \n\nDo not reply to this mail" | mail -s "$ADMINDOMAIN new passwd for user $USERNAME" "$ADMINMAIL,$EMAIL"
 
@@ -91,6 +110,7 @@ read USERNAME
 id $USERNAME > /dev/null 2>&1
 if [ "$?" != 0 ]
 then
+
 echo "User already removed or not valid"
         exit 1
 fi
@@ -103,7 +123,8 @@ rm /etc/httpd/conf/$DOMEIN.conf
 
 echo "user: $USERNAME has been removed from the server"
 
-#remove # if you want to recieve mail that the user is removed # | mail -s "$ADMINDOMAIN user removed" "$ADMINMAIL"
+#remove # if you want to recieve mail that the user is removed
+# | mail -s "$ADMINDOMAIN user removed" "$ADMINMAIL"
 
 }
 
@@ -123,6 +144,7 @@ then
 echo "User not valid"
         exit 1
 fi
+
 echo -n "also send an email to:"
 read EMAIL
 echo -n "domein:"
@@ -144,8 +166,43 @@ usermod -p $(openssl passwd -crypt $GENPASS) $USERNAME
 echo "username: $USERNAME"
 echo "passwd: $GENPASS"
 
-#remove als the # to recieve mail with the new password # #echo -e "Hi there, \n\nThis is your new password for your ftp account at $DOMEIN \n\nusername:$USERNAME \npasswd: $GENPASS \n\n$ADMINNAME \n$ADMINDOMAIN \n\nIn case of lost password, send  an #e-mail to $ADMINMAIL \n #You are fully responsible for any information or file supplied on your #website. Is not Allowed to publish any copyrighted material that is not owned #by yourself. Is not allowed to post any information which is vulgar, #harassing, hateful, threatening, invading of others privacy, sexually #oriented, or violates any laws, child porn would be reported to the police.
-#Just be nice & enjoy the net. \n\nDo not reply to this mail" | mail -s "$ADMINDOMAIN new passwd for # user $USERNAME" "$ADMINMAIL,$EMAIL"
+echo "User not valid"
+        exit 1
+fi
+
+echo -n "also send an email to:"
+read EMAIL
+echo -n "domein:"
+read DOMEIN
+
+#
+# Generate a password for the user
+#
+MATRIX="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+LENGTH="8"
+while [ ${n:=1} -le $LENGTH ]
+do
+GENPASS="$GENPASS${MATRIX:$(($RANDOM%${#MATRIX})):1}"
+        let n+=1
+done
+
+usermod -p $(openssl passwd -crypt $GENPASS) $USERNAME
+
+echo "username: $USERNAME"
+echo "passwd: $GENPASS"
+
+#remove als the # to recieve mail with the new password
+#
+#echo -e "Hi there, \n\nThis is your new password for your ftp account at $DOMEIN \n\nusername:$USERNAME \npasswd: $GENPASS \n\n$ADMINNAME \n$ADMINDOMAIN \n\nIn case of lost password, send
+ an
+#e-mail to $ADMINMAIL \n
+#You are fully responsible for any information or file supplied on your
+#website. Is not Allowed to publish any copyrighted material that is not owned
+#by yourself. Is not allowed to post any information which is vulgar,
+#harassing, hateful, threatening, invading of others privacy, sexually
+#oriented, or violates any laws, child porn would be reported to the police.
+#Just be nice & enjoy the net. \n\nDo not reply to this mail" | mail -s "$ADMINDOMAIN new passwd for
+# user $USERNAME" "$ADMINMAIL,$EMAIL"
 
 }
 case "$1" in
@@ -165,4 +222,3 @@ case "$1" in
         ;;
 esac
 exit 0
-
